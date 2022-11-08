e514099dabe42907266ff26b613ca9fe2018b9c6
Dario-Miccinelli <poduzzo@hotmail.com>
2022-11-08 14:04:39 +0100

    Creato array bike

--- Snack 3/assets/js/main.js
+++ Snack 3/assets/js/main.js
@@ -1,2 +1,25 @@
 // Snack 3
 // Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+
+const bike = [
+    {
+        nome : "Bike Baby",
+        peso : 190,
+    },
+    {
+        nome : "Bike Light",
+        peso : 100,
+    },
+    {
+        nome : "Bike Enduro",
+        peso : 150,
+    },
+    {
+        nome : "Bike Run",
+        peso : 140,
+    },
+    {
+        nome : "Bike City",
+        peso : 110,
+    },
+]
